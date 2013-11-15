fc3623b2bc80ceef07bb18ab6eca8651b2a29a71
冯乐乐 <lelefeng1992@gmail.com>
2013-11-15 16:37:50 +0800

    [add] add NGUI, initial the project

---
+++ Virtual SQL/Assets/VirtualSQL/Editor/VirtualSQLEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class VirtualSQLEditor {
+
+	[MenuItem("Virtual SQL/Add a Table")]
+	static void addTable() {
+		// parse settings from json
+		System.IO.TextWriter w = new System.IO.StreamWriter(UnityEngine.Application.dataPath + "/Resources/VirtualSQL Data/family");
+		
+		// build head of initializer
+		w.WriteLine("// DON'T MODIFY THIS FILE MANUALLY SINCE IT WAS GENERATED FROM NETWORK TOOL CHAIN!");
+		
+		w.Flush();
+		
+		w.Close();
+	}
+}
